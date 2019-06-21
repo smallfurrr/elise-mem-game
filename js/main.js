22878f26f5bd01d104a3878cf5038a9001c42267
EliseChia <swedesurrender@gmail.com>
2019-06-21 15:43:35 +0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+const cardOne = 'queen';
+const cardTwo = 'king';
+const cardThree = 'queen';
+const cardFour = 'king';
+
+console.log(`User flipped ${cardOne}.`);
+console.log(`User flipped ${cardTwo}.`);
