821a2996c9cf828ec812547392df7a78df4b2495
EliseChia <swedesurrender@gmail.com>
2019-06-22 18:33:17 +0800

    finished last steps of game

--- js/main.js
+++ js/main.js
@@ -1,45 +1,62 @@
-const cards = [
-{
+const cards = [{
     rank: 'queen',
     suit: 'hearts',
     cardImage: 'images/queen-of-hearts.png'
-},
-{
+}, {
     rank: 'queen',
     suit: 'diamonds',
     cardImage: 'images/queen-of-diamonds.png'
-},
-{
+}, {
     rank: 'king',
-    suit:'hearts',
+    suit: 'hearts',
     cardImage: 'images/king-of-hearts.png'
-},
-{
+}, {
     rank: 'king',
     suit: 'diamonds',
     cardImage: 'images/king-of-diamonds.png'
-}
-];
+}];
 
 let cardsInPlay = [];
 
+function resetDeck() {
+    var board = document.getElementById('game-board');
+    while (board.hasChildNodes()) {
+        board.removeChild(board.firstChild)
+    }
+    createBoard()
+    cardsInPlay = [];
+}
+
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
     } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
     }
+    resetDeck();
 }
 
-function flipCard(cardID) {
-    console.log(`${cards[cardID].rank} was flipped!`);
-    console.log(cards[cardID].cardImage);
-    console.log(cards[cardID].suit);
+function flipCard() {
+    var cardID = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardID].cardImage);
     cardsInPlay.push(cards[cardID].rank);
     if (cardsInPlay.length === 2) {
-      checkForMatch();
+        setTimeout(function() {
+          checkForMatch();
+        }, 100);
     }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
+
+//to-do: add score feature!
